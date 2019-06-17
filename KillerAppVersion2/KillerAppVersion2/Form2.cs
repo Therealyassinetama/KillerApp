737b0f92338ebe4220f47490712dd1b403bb4e3d
Therealyassinetama <49792918+Therealyassinetama@users.noreply.github.com>
2019-06-17 20:21:35 +0200

    Add files via upload

---
+++ KillerAppVersion2/KillerAppVersion2/Form2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KillerAppVersion2
+{
+    public partial class Form2 : Form
+    {
+
+        private int selectedset;
+        private int selectedserie;
+
+        public Form2(string username, string password)
+        {
+            Set set1 = new Set("Legendary eagle");
+            sets.Add(set1);
+            InitializeComponent();
+            foreach (Card card in sets[0].series[0].cards)
+            {
+                listBox1.Items.Add(card.ToString());
+            }
+            foreach (Serie serie in sets[0].series)
+            {
+                listBox2.Items.Add(serie.ToString());
+            }
+            foreach (Set set in sets)
+            {
+                listBox3.Items.Add(set.ToString());
+            }
+        }
+        List<Set> sets = new List<Set>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateListbox();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedserie = listBox2.SelectedIndex;
+            UpdateListbox();
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedset = listBox3.SelectedIndex;
+            UpdateListbox();
+        }
+
+        public void UpdateListbox()
+        {
+            try
+            {
+                ClearListbox();
+                foreach (Card card in sets[selectedset].series[selectedserie].cards)
+                {
+                    listBox1.Items.Add(card);
+                }
+                foreach (Serie serie in sets[selectedset].series)
+                {
+                    listBox2.Items.Add(serie);
+                }
+                foreach (Set set in sets)
+                {
+                    listBox3.Items.Add(set);
+                    listBox3.Items.Add(set);
+                    listBox3.Items.Add(set);
+                    listBox3.Items.Add(set);
+                    listBox3.Items.Add(set);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Alleen op tekst drukken AUB");
+                foreach (Card card in sets[0].series[0].cards)
+                {
+                    listBox1.Items.Add(card.ToString());
+                }
+                foreach (Serie serie in sets[0].series)
+                {
+                    listBox2.Items.Add(serie.ToString());
+                }
+                foreach (Set set in sets)
+                {
+                    listBox3.Items.Add(set.ToString());
+                }
+            }
+        }
+
+        public void ClearListbox()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+        }
+    }
+}
---
+++ KillerAppVersion2/KillerAppVersion2/Serie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillerAppVersion2
+{
+    class Serie
+    {
+        public Serie(string serienr)
+        {
+            name = serienr;
+            for (int i = 1; i <= 10; i++)
+            {
+                Card card = new Card(("card " + i.ToString()));
+                cards.Add(card);
+            }
+        }
+
+        public string name { get; set; }
+        public int value { get; set; }
+
+
+
+        public List<Card> cards = new List<Card>();
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
